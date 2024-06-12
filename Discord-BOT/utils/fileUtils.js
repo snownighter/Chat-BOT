59bc55fe5d0fac62d48bb41b162302c7a648cb98
Snighter <60771502+snownighter@users.noreply.github.com>
2024-06-13 00:26:58 +0800

    Update fileUtils.js

--- Discord-BOT/utils/fileUtils.js
+++ Discord-BOT/utils/fileUtils.js
@@ -1,29 +1,29 @@
 // utils/fileUtils.js
 
-const fs = require('fs')
+const fs = require('fs');
 
 // 讀取檔案的 async function
 async function readFileAsync(filePath) {
     try {
-        const data = await fs.promises.readFile(filePath, 'utf8')
-        return JSON.parse(data)
+        const data = await fs.promises.readFile(filePath, 'utf8');
+        return JSON.parse(data);
     } catch {
         // 如果檔案不存在或讀取失敗，回傳空的物件
-        return {}
+        return {};
     }
 }
 
 // 寫入檔案的 async function
 async function writeFileAsync(filePath, data) {
     try {
-        await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2))
+        await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2));
     } catch (err) {
-        console.error(err)
-        throw err
+        console.error(err);
+        throw err;
     }
 }
 
 module.exports = {
     readFileAsync,
     writeFileAsync,
-}
+};
